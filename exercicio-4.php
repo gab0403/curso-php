a30c330965417e0294407a69c606b8ab7b9ee1ae
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-11-07 13:07:48 -0300

    feat: exercícios 4, 5, 6 e 7 - pratica tipos de dados

---
+++ exercicio-4.php
@@ -0,0 +1,9 @@
+<?php
+$numberFloat1 = 2.99
+$numberFloat2 = 5129763.514514
+$numberFloat3 = 4.6666
+
+echo $numberFloat1
+echo $numberFloat2
+echo $numberFloat3
+?>
---
+++ exercicio-6.php
@@ -0,0 +1,11 @@
+<?php
+
+$arr = ['carro' => 'Ford', 'cor' => 'branco', 'portas' => 5];
+
+print_r($arr)
+
+echo $arr['carro'];
+echo $arr['cor'];
+echo $arr['portas'];
+
+?>
