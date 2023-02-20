90a6f5d9abf1283060ee9d4d78bbd995e3c81cda
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2023-02-20 16:32:54 -0300

    feat: login do usuario parte 1

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -86,7 +86,27 @@ class UserDAO implements UserDAOInterface {
     }
 
     public function authenticateUser($email, $password){
+        
+        $user = $this->findByEmail($email);
+        if ($user) {
+            
+            if (password_verify($password, $user->password)) {
+                
+                $token = $user->generateToken();
+                $this->setTokenToSession($token);
 
+                $user->token = $token;
+                $this->update($user);
+
+                return true;
+
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
     }
     public function findByEmail($email){
 
@@ -142,7 +162,7 @@ class UserDAO implements UserDAOInterface {
     }
 
     public function changePassword(User $user){
-
+        
     }
 
 
